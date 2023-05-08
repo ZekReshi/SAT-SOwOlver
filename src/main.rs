68e258ff79a46611653965cba1fa6232e324ef8b
Maximilian Wahl <mexx.wale@gmail.com>
2023-05-08 11:54:47 +0200

    add formula reading from file

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,35 @@
 mod sowolver;
 
+use std::{fs::File, io::{BufRead,BufReader}};
+
 use crate::sowolver::SOwOlver;
 
 fn main() {
-    let mut s = SOwOlver::new(5, 3);
-    s.add(1);
-    s.add(-2);
-    s.add(0);
-    s.add(4);
-    s.add(-5);
-    s.add(0);
-    s.add(2);
-    s.add(-3);
-    s.add(4);
-    s.add(0);
-    s.assume(1);
-    s.assume(-5);
+    let file = File::open("formula.cnf").expect("cannot read file");
+    let mut file = BufReader::new(file);
+    let mut first_line = String::new();
+    let _ = file.read_line(&mut first_line);
+    let mut varCount = 0;
+    let mut clauseCount = 0;
+    for (idx,val) in first_line.split_ascii_whitespace().enumerate(){
+        if idx == 2 {
+            varCount = val.parse::<usize>().expect("file malformed");
+        }
+        if idx == 3 {
+            clauseCount = val.parse::<usize>().expect("file malformed");
+        }
+    }
+    let mut s = SOwOlver::new(varCount, clauseCount);
+
+
+        for line in file.lines().filter_map(|result| result.ok()){
+            for val in line.split_ascii_whitespace() {
+                let lit = val.parse::<i128>().expect("file malformed");
+                s.add(lit.abs().try_into().unwrap(),lit >0);
+            }
+            
+    
+    }
     println!("{:?}", s.vars);
-    println!("{:?}", s.clauses);
+    println!("{:?}", s.clauses.len());
 }
